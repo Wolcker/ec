949583ae349b117017c0b841f0869dd224308564
Wolcker <leonard_von_wolcker@hotmail.com>
2022-03-11 21:34:11 +0100

    styled the search box, inside the component.

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -1,11 +1,28 @@
+import styled from "styled-components";
+
 const SearchBar = ({ value, onChange }) => {
   return (
-    <input
-      value={value}
-      onChange={(e) => onChange(e.currentTarget.value)}
-      placeholder="Search"
-    />
+    <Div>
+      <Input
+        value={value}
+        onChange={(e) => onChange(e.currentTarget.value)}
+        placeholder="Search"
+      />
+    </Div>
   );
 };
 
 export default SearchBar;
+
+const Input = styled.input`
+  width: 97%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+`;
+
+const Div = styled.div`
+  text-align: center;
+  margin: 32px;
+  border-radius: 8px;
+`;
